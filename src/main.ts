a5878c0755378ba7f38a1a133058f7a7a4dd5167
KJH <stonehead334@gmail.com>
2022-05-31 16:35:32 +0900

    Validating ArgsTypes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 /* AppModule은 main.ts로 import 되는 유일한 모듈이다 */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //pipelines
   await app.listen(3000);
 }
 bootstrap();
--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,14 +1,25 @@
 import {Field, InputType, ArgsType} from '@nestjs/graphql';
+import { IsString, IsBoolean } from 'class-validator';
 
 // ArgsType - 아래의 정의된 것들을 분리된 argument로써 정의할 수 있다
+//validator 유효성 검사를 하려면 pipeline을 설치해야 하는 걸 잊지말자
 @ArgsType() 
 export class createRestaurantDto {
+
     @Field(type => String)
+    @IsString()
+    
      name: string;
+
     @Field(type => Boolean)
+    @IsBoolean()
      isVegan: boolean;
+
     @Field(type => String)
+    @IsString()
     address: string;
+
     @Field(type => String)
+    @IsString()
      ownersName: string;
 } 
