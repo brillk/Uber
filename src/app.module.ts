6d6da7f6d046ded75b7d4d81b25c45965ed164ce
KJH <stonehead334@gmail.com>
2022-06-02 09:39:00 +0900

    ConfigService and TypeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,22 @@ import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 // 항상 GraphQL을 만들때는 resolvers와 query를 생성해야 한다
+// env파일을 test, production, development로 나눠서 생성
+/*
+Configuration
+응용 프로그램은 종종 다른 환경에서 실행됩니다. 환경에 따라 다른 구성 설정을 사용해야 합니다.
+Nest에서 이 기술을 사용하는 좋은 방법은 적절한 .env 파일을 로드하는 ConfigService를 노출하는 ConfigModule을 만드는 것입니다.
+npm i @nestjs/config --save
+*/
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env",
+    }),
     TypeOrmModule.forRoot({
       type: "postgres",
       host: "localhost",
