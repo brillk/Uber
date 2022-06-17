552f05e6d821c45a510b38483f913452da42888e
KJH <stonehead334@gmail.com>
2022-06-17 13:50:22 +0900

    Module E2E Setup part-2

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -14,11 +15,18 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-it.todo("hi");
-it.todo("me");
-it.todo("userProfile");
-it.todo("createAccount");
-it.todo("login");
-it.todo("editProfile");
-it.todo("verifyEmail");
+  //뭔가가 종료되지 않은 상태에서 jest가 종료되었기에 
+  // 뜨는 경고 해결
+  // 테스트가 끝나면 데이터베이스를 드랍할거다
+  afterAll(async() => {
+    await getConnection().dropDatabase()
+    app.close();
+  }) 
+
+  it.todo("createAccount");
+  it.todo("userProfile");
+  it.todo("login");
+  it.todo("me");
+  it.todo("verifyEmail");
+  it.todo("editProfile");
 });
