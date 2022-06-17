00c3b27b36d62ccc21a0b1b728913002063cd77e
KJH <stonehead334@gmail.com>
2022-06-17 10:19:08 +0900

    jwt Service Test Setup

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -7,7 +7,8 @@ import { JwtModuleOptions } from './jwt.interfaces';
 export class JwtService {
     constructor(
         @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions
-    ) {} // providers에서 필요한 걸 적고 nestjs에게 요청한다
+    ) {
+    } // providers에서 필요한 걸 적고 nestjs에게 요청한다
     sign(userId: number): string {
         return jwt.sign({id:userId}, this.options.privateKey);
     }
