763bb10136176565ffffd53f9d9040f3683dbc1d
KJH <stonehead334@gmail.com>
2022-06-17 10:45:30 +0900

    jwt Verify Test & setup for todo

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -2,15 +2,20 @@ import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service'
 import * as jwt from "jsonwebtoken";
+
+
+const TEST_KEY = 'testKey';
+const USER_ID = 1;
+
 //mock할 module을 부르는 방법
 jest.mock('jsonwebtoken', () => {
     return {
         sign: jest.fn(() => 'TOKEN'),
+        verify: jest.fn(() => ({ id: USER_ID })),
     }
 })
 
 
-const TEST_KEY = 'testKey';
 
 describe('JwtService', () => {
 
@@ -32,16 +37,20 @@ describe('JwtService', () => {
 
     describe("sign", () => {
         it("should return a signed token", () => {
-            const ID = 1;
-            const token = service.sign(ID);
+            const token = service.sign(USER_ID);
             expect(typeof token).toBe('string');
             expect(jwt.sign).toHaveBeenCalledTimes(1);
-            expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+            expect(jwt.sign).toHaveBeenLastCalledWith({ id: USER_ID }, TEST_KEY);
         })
     })
     describe("verify", () => {
         it("should return the decoded token", () => {
-    
+            const TOKEN = "TOKEN"
+            const decodedToken = service.verify(TOKEN);
+            expect(decodedToken).toEqual({id: USER_ID});
+            expect(jwt.verify).toHaveBeenCalledTimes(1);
+            expect(jwt.verify).toHaveBeenLastCalledWith(TOKEN, TEST_KEY);
+       
         })
     })
 });
