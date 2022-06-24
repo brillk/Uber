e443620b858ff896a56f523c00c3e137a0e5f0da
KJH <stonehead334@gmail.com>
2022-06-24 11:44:20 +0900

    Delete Restaurant

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
+import { DeleteRestaurantInput, DeleteRestaurantOutput } from './dtos/delete-restaurant.dto';
 import { EditRestaurantInput, EditRestaurantOutput } from './dtos/edit-restaurant.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
@@ -86,6 +87,35 @@ export class RestaurantService {
                 }
             }
         }
+
+        async deleteRestaurant(
+            owner: User, 
+            {restaurantId}: DeleteRestaurantInput,
+            ): Promise<DeleteRestaurantOutput> {
+                try {
+                    const restaurant  = await this.restaurants.findOne(
+                        restaurantId, 
+                        );
+                    if(!restaurant) {
+                        return {
+                            ok: false,
+                            error: "Restaurant not found",
+                        }
+                    } 
+                    if(owner.id !== restaurant.ownerId) {
+                        return {
+                            ok: false,
+                            error: "You can't delete a restaurant that you don't own",
+                        }
+                    }
+                    await this.restaurants.delete(restaurantId);
+                } catch {
+                    return {
+                        ok: false,
+                        error: "Couldn't delete"
+                    }
+                }
+            }
 }
 
 //role base authentication
